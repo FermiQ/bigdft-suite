bdabbfd924ca4b5715ffbf169ad75351088c355a
maxime-moriniere <maxime.moriniere@hotmail.fr>
2017-04-06 16:59:33 -0400

    Major modifications to fund_files in Utils.py for futile.

--- futile/src/python/futile/Utils.py
+++ futile/src/python/futile/Utils.py
@@ -4,9 +4,10 @@ This file contains some low-level useful functions
 
 from __future__ import print_function
 
+
 def find_files(regexp, archive=None):
     """
-    Returns a list of all the paths to files that follow the regular expression
+    Returns a list of the paths to the files that follow the regular expression
     regexp. They are searched from the current working directory or from an archive 
     given as optional argument.
 
@@ -30,14 +31,23 @@ def find_files(regexp, archive=None):
 
         #Example using a tarfile
         import tarfile
-        my_archive = tarfile,open('archive.tar.gz')
+        my_archive = tarfile.open('archive.tar.gz')
         find_files('*/*/log-*.yaml', archive=my_archive)
     """
+    import os
 
-    if archive:
-        return _find_files_from_archive(regexp, archive) 
+    #Get a list of all paths to files satisfying the regexp
+    if archive is not None:
+        paths = _find_files_from_archive(regexp, archive) 
     else:
-        return _find_files_from_current_folder(regexp)
+        paths = os.popen('ls '+regexp).read().splitlines()
+
+    #Test that the regexp found files
+    if paths == []:
+        raise ValueError('The regexp "{}" leads to no file. '\
+                         'Consider using another one.'.format(regexp))
+    else:
+        return paths
 
 
 def _find_files_from_archive(re, archive):
@@ -47,62 +57,55 @@ def _find_files_from_archive(re, archive):
     #function to identify an archive out of its regexp, 
     #solves the bug in re for '*' (solved in Python 2.7.6)
     """
-    #Maybe some
-    #from ../../../../bigdft/src/python/BigDFT import Logfiles as lf 
-    #from futile import Yaml
-    result=[]
-    for f in archive.getnames():
-        isthere=True
-        for pattern in re.split('*'):
-            isthere = isthere and (pattern in f)
-        if isthere: 
-            #print(f)
-            filetar = archive.extractfile(f)
-            content = filetar.read()
-            dictionary = Yaml.yaml.load(stream=content)
-            result.append(lf.Logfile(dictionary=dictionary))
-    return result
+    import tarfile    
 
-
-def _find_files_from_current_folder(regexp):
-    """
-    Returns a list of all the files paths that follow the regular expression
-    regexp in the current working directory.
-    """
-    import os
-    paths = os.popen('ls '+regexp).read().splitlines()
-    #print(type(paths), paths)
-    if paths == []:
-        raise ValueError('The regexp "{}" leads to no file. '\
-                         'Consider using another one.'.format(regexp))
-    else:
-        return paths
-
+    #Open the archive
+    with tarfile.open(archive, 'r') as arch:
+    #Return paths to logfiles satisfying the regexp
+    return [f for f in arch.getnames() 
+            if all(pattern in f for pattern in re.split('*'))]
 
 
 
 if __name__ == '__main__':
+    import os
+
     #Tests of the find_files function
+    #
+    print("Test finding all python files in this directory")
     print(find_files("*py"))
+    print()
+
+    #
+    print("Test finding the Utils.py file in this directory")
     print(find_files("Utils.py"))
+    print()
+ 
+    #
+    print("Test raising a ValueError because the regexp leads to no files")
     try:
         find_files('*html')
     except ValueError as e:
         print('This raised the following ValueError:')
         print(e)
+    print()
+
+    #
+    print("Test raising an exception because there is no such archive.")
+    fname = 'file.tar.gz'
+    if fname in os.popen('ls'): os.system('rm '+fname)
+    #os.system('rm '+fname)
     try:
-        find_files('*py', archive='file.tar.gz')
+        find_files('*py', archive=fname)
     except Exception as e:
         #print(dir(e))
         print('This raised the following Exception:')
         print(e)
-    import os
-    fname = 'file.tar.gz'
+    print()
+
+    #
+    print("Test without error using an archive")
     os.system('find * -name "*py" | tar -zcvf '+fname+' -T -')
     os.system('ls '+fname)
-    import tarfile
-    my_archive = tarfile.open(fname)
-    ##not working at present, need to import Logfile and Yaml
-    ##in _find_files_from_archive
-    #find_files('*py', archive=my_archive)
+    find_files('*py', archive=fname)
     os.system('rm '+fname)
