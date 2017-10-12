0f8a5a9ea486e061592a0d4c37843776a0d12049
Stephan Mohr <stephan.mohr@bsc.es>
2017-10-12 20:27:37 +0200

    added a keyword to decide whether the entropy contribution shall be included or not

--- bigdft/src/linear/linearScaling.f90
+++ bigdft/src/linear/linearScaling.f90
@@ -792,21 +792,23 @@ subroutine linearScaling(iproc,nproc,KSwfn,tmb,at,input,rxyz,denspot,rhopotold,n
 
     call check_for_exit()
     if(exit_outer_loop) then
-        ! Calculate the entropy contribution to the energy
-        if (iproc==0) then
-            call yaml_comment('Calculate entropy contribution',hfill='-')
-            call yaml_mapping_open('Calculate entropy contribution')
-        end if
-        call calculate_entropy_term(iproc, nproc, bigdft_mpi%mpi_comm, &
-             foe_data_get_real(tmb%foe_obj,"fscale"), &
-             tmb%linmat%smat(1), tmb%linmat%smat(3), &
-             tmb%linmat%ovrlp_, tmb%linmat%kernel_, tmb%linmat%ovrlppowers_(2), &
-             input%cp%foe%accuracy_entropy, energs%eTS)
-         energy = energy - energs%eTS
-         if (iproc==0) then
-             call write_energies(0,energs,0.d0,0.d0,'FINAL',only_energies=.true.)
-             call yaml_map('energy',energy)
-             call yaml_mapping_close()
+        if (input%lin%consider_entropy) then
+            ! Calculate the entropy contribution to the energy
+            if (iproc==0) then
+                call yaml_comment('Calculate entropy contribution',hfill='-')
+                call yaml_mapping_open('Calculate entropy contribution')
+            end if
+            call calculate_entropy_term(iproc, nproc, bigdft_mpi%mpi_comm, &
+                 foe_data_get_real(tmb%foe_obj,"fscale"), &
+                 tmb%linmat%smat(1), tmb%linmat%smat(3), &
+                 tmb%linmat%ovrlp_, tmb%linmat%kernel_, tmb%linmat%ovrlppowers_(2), &
+                 input%cp%foe%accuracy_entropy, energs%eTS)
+             energy = energy - energs%eTS
+             if (iproc==0) then
+                 call write_energies(0,energs,0.d0,0.d0,'FINAL',only_energies=.true.)
+                 call yaml_map('energy',energy)
+                 call yaml_mapping_close()
+             end if
          end if
          exit outerLoop
      end if
