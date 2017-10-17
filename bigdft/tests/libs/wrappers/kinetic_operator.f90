66bedbb1ac29123368f54357202787ad758cc108
Giuseppe Fisicaro <fisicaro@maia.cluster.bc2.ch>
2017-10-17 11:01:35 +0200

    test for kinetic operator working well

--- bigdft/tests/libs/wrappers/kinetic_operator.f90
+++ bigdft/tests/libs/wrappers/kinetic_operator.f90
@@ -1,5 +1,5 @@
 !> test program for the function projection in wavelets
-program projection
+program kinetic_operator
   use module_defs, only: UNINITIALIZED
   use futile
   use box
@@ -50,8 +50,8 @@ program projection
   hgrids=options//'hgrid'
   sigma=options//'sigma'
   dict_posinp=f_tree_load('{positions: [{ C: [0.0, 0.0, 0.0]}], cell: [10,15,11]}')
-  crmult=10.0_f_double
-  frmult=10.0_f_double
+  crmult=50.0_f_double
+  frmult=50.0_f_double
   angrad=onehalf*pi
   oxyz=5.0_f_double
   kpoint=0.0_f_double
@@ -135,16 +135,19 @@ program projection
 
   call initialize_work_arrays_locham(lr,nspinor,.true.,wl)
   call f_zero(hpsi)
-  call f_zero(psir)
 
   hpsi=tpsi
+
+  call daub_to_isf_locham(nspinor,lr,wl,hpsi,psir)
+
+  call f_zero(psir)
 
   call isf_to_daub_kinetic(hx,hy,hz,kx,ky,kz,nspinor,lr,wl,psir,hpsi,ekin,k_strten)
 
   !build up the analytical kinetic operator applyed to a gaussian
   call f_zero(gaussian)
   oxyz=lr%mesh%hgrids*[lr%nsi1,lr%nsi2,lr%nsi3]
-  call real_space_gaussian(ncplx_g,n,l,ider,nterm_max,coeff,expo,rxyz,oxyz,lr,gaussian)
+  call real_space_laplacian(ncplx_g,n,l,ider,nterm_max,coeff,expo,rxyz,oxyz,lr,gaussian)
   call isf_to_daub(lr,w,gaussian,psi)
 
   !calculate the difference of the two arrays
@@ -182,7 +185,7 @@ program projection
            1,lr,wp,psi,method=method)
     end subroutine project    
 
-end program projection
+end program kinetic_operator
 
     subroutine real_space_gaussian(ncplx_g,n,l,ider,nterm_max,coeff,expo,rxyz,oxyz,lr,gaussian)
       use gaussians
@@ -221,7 +224,7 @@ end program projection
       bit=box_iter(lr%mesh)
       call three_dimensional_density(bit,g,sqrt(lr%mesh%volume_element),rxyz,gaussian)
 
-!!$      !for the moment only with s projectors (l=0,n=1)
+      !for the moment only with s projectors (l=0,n=1)
 !!$      noxyz=rxyz-oxyz
 !!$
 !!$      bit=box_iter(lr%mesh,origin=noxyz) !use here the real space mesh of the projector locreg
@@ -239,5 +242,63 @@ end program projection
 !!$         !     lr%mesh%hgrids*[lr%nsi1,lr%nsi2,lr%nsi3],&
 !!$         !     lr%mesh_coarse%hgrids*[lr%ns1,lr%ns2,lr%ns3],rxyz,noxyz
 !!$         call separable_3d_function(bit,funcs,factors(1,1,m)*sqrt(lr%mesh%volume_element),gaussian)
+!!$         !call separable_3d_laplacian(bit,funcs,factors(1,1,m)*sqrt(lr%mesh%volume_element),gaussian)
 !!$      end do !not correctly written, it should be used to define the functions
     end subroutine real_space_gaussian
+
+
+    subroutine real_space_laplacian(ncplx_g,n,l,ider,nterm_max,coeff,expo,rxyz,oxyz,lr,gaussian)
+      use gaussians
+      use futile
+      use locregs
+      use box
+      use f_functions
+      implicit none
+      integer, intent(in) :: ncplx_g !< 1 or 2 if the gaussian factor is real or complex respectively
+      integer, intent(in) :: n !<principal quantum number
+      integer, intent(in) :: l !<angular momentum of the shell
+      integer, intent(in) :: ider !<direction in which to perform the derivative (0 if any)
+      integer, intent(in) :: nterm_max !if GTH nterm_max=4 (this value should go in a module)
+      real(f_double), dimension(ncplx_g), intent(in) :: coeff !<prefactor of the gaussian
+      real(f_double), dimension(ncplx_g), intent(in) :: expo 
+      real(f_double), dimension(3), intent(in) :: rxyz,oxyz 
+      type(locreg_descriptors), intent(in) :: lr
+      real(f_double), dimension(lr%mesh%ndim), intent(out) :: gaussian
+      ! Local variables
+      integer, dimension(2*l-1) :: nterms
+      integer, dimension(nterm_max,3,2*l-1) :: lxyz
+      real(f_double), dimension(ncplx_g) :: sigma_and_expo
+      real(f_double), dimension(ncplx_g,nterm_max,2*l-1) :: factors
+      integer :: m,i
+      type(box_iterator) :: bit
+      type(f_function), dimension(3) :: funcs
+      type(gaussian_real_space) :: g
+      real(f_double), dimension(3) :: noxyz 
+
+      call get_projector_coeffs(ncplx_g,l,n,ider,nterm_max,coeff,expo,&
+           nterms,lxyz,sigma_and_expo,factors)
+
+      !call gaussian_real_space_set(g,sqrt(onehalf/expo(1)),1,factors,lxyz)
+!      call gaussian_real_space_set(g,sigma_and_expo(1),1,factors,lxyz(1,:,1),[0],16)
+!      call f_zero(gaussian)
+!      bit=box_iter(lr%mesh)
+!      call three_dimensional_density(bit,g,sqrt(lr%mesh%volume_element),rxyz,gaussian)
+
+      !for the moment only with s projectors (l=0,n=1)
+      noxyz=rxyz-oxyz
+
+      bit=box_iter(lr%mesh,origin=noxyz) !use here the real space mesh of the projector locreg
+      do m=1,2*l-1
+         do i=1,3
+            funcs(i)=f_function_new(f_gaussian,exponent=expo(1))
+         end do
+
+         !here we do not consider the lxyz terms yet
+         !take the reference functions
+         !print *,size(gaussian),'real',lr%mesh%ndims,&
+         !     lr%mesh%hgrids*[lr%nsi1,lr%nsi2,lr%nsi3],&
+         !     lr%mesh_coarse%hgrids*[lr%ns1,lr%ns2,lr%ns3],rxyz,noxyz
+         !call separable_3d_function(bit,funcs,factors(1,1,m)*sqrt(lr%mesh%volume_element),gaussian)
+         call separable_3d_laplacian(bit,funcs,-2.0_f_double*factors(1,1,m)*sqrt(lr%mesh%volume_element),gaussian)
+      end do !not correctly written, it should be used to define the functions
+    end subroutine real_space_laplacian
