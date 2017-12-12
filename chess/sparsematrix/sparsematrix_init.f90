b725c4face8e5b54ae3492d1b2a030fbe90fd6fe
Stephan Mohr <stephan.mohr@bsc.es>
2017-12-12 16:51:19 +0100

    small correction

--- chess/sparsematrix/sparsematrix_init.f90
+++ chess/sparsematrix/sparsematrix_init.f90
@@ -5923,13 +5923,13 @@ module sparsematrix_init
      ind_min = smat%nvctr
      ind_max = 0
 
-     ! The matrix as object of a matrix multiplication
-     ind_min = min(ind_min,smat%smmm%isvctr_mm+1)
-     ind_max = max(ind_max,smat%smmm%isvctr_mm+smat%smmm%nvctrp_mm)
 
      call check_compress_distributed_layout(smat,ind_min,ind_max)
      if (smat%smatmul_initialized) then
          call check_matmul_layout(smat%smmm%nseq,smat%smmm%indices_extract_sequential,ind_min,ind_max)
+         ! The matrix as object of a matrix multiplication
+         ind_min = min(ind_min,smat%smmm%isvctr_mm+1)
+         ind_max = max(ind_max,smat%smmm%isvctr_mm+smat%smmm%nvctrp_mm)
      end if
      call check_ortho_inguess(smat,ind_min,ind_max)
      !!call check_projector_charge_analysis(iproc, nproc, smmd, smat, ind_min, ind_max)
