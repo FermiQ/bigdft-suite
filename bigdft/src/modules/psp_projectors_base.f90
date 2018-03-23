4a73ceeeccc6c152ec95ed52ffcbcf808b0a10db
Damien Caliste <damien.caliste@cea.fr>
2018-03-23 16:13:27 +0100

    Correct tail treatment for null projectors like H.

--- bigdft/src/modules/psp_projectors_base.f90
+++ bigdft/src/modules/psp_projectors_base.f90
@@ -674,9 +674,11 @@ contains
        nlpsp%projs(iat)%region%plr%mesh%ndims = lr%mesh%ndims
        call f_free_ptr(nlpsp%projs(iat)%region%lut_tolr)
        call free_tolr_ptr(nlpsp%projs(iat)%region%tolr)
-       call set_wfd_to_wfd(lr, nlpsp%projs(iat)%region%plr, &
-            & keyg_lin, nbsegs_cf, nlpsp%projs(iat)%region%noverlap, &
-            & nlpsp%projs(iat)%region%lut_tolr, nlpsp%projs(iat)%region%tolr)
+       if (nseg > 0) then
+          call set_wfd_to_wfd(lr, nlpsp%projs(iat)%region%plr, &
+               & keyg_lin, nbsegs_cf, nlpsp%projs(iat)%region%noverlap, &
+               & nlpsp%projs(iat)%region%lut_tolr, nlpsp%projs(iat)%region%tolr)
+       end if
     end do
     call f_free(keyg_lin)
     call f_free(nbsegs_cf)
