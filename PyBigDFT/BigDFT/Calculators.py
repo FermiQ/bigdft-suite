b36361e2401a6380648d899fe799afefd23b9c88
Thierry Deutsch <Thierry.Deutsch@cea.fr>
2018-06-14 16:45:15 +0200

    Add posinp key into dictionary in the run method of SystemCalculator

--- PyBigDFT/BigDFT/Calculators.py
+++ PyBigDFT/BigDFT/Calculators.py
@@ -1,5 +1,33 @@
 """file for BigDFT calculators"""
 
+#The goal is to have a light Calculator almost compatible with ASE (Atomic Simulation environment, see https://gitlab.com/ase/ase)
+#In a future we add our method to ASE which is at a higher level (workflow of simulations)
+# from ase import Atoms
+# from ase.optimize import BFGS
+# from ase.calculators.nwchem import NWChem
+# from ase.io import write
+# h2 = Atoms('H2',
+#            positions=[[0, 0, 0],
+#                       [0, 0, 0.7]])
+# h2.calc = NWChem(xc='PBE')
+# opt = BFGS(h2, trajectory='h2.traj')
+# opt.run(fmax=0.02)
+# BFGS:   0  19:10:49    -31.435229     2.2691
+# BFGS:   1  19:10:50    -31.490773     0.3740
+# BFGS:   2  19:10:50    -31.492791     0.0630
+# BFGS:   3  19:10:51    -31.492848     0.0023
+# write('H2.xyz', h2)
+# h2.get_potential_energy()  # ASE's units are eV and Ang
+# -31.492847800329216
+
+#In our case for the class SystemCalculator which uses system calls:
+#We define posinp (equivalent of Atoms)
+#We have a python dictionary for the parameter
+#We define a calculator (equivalent of BFGS which is an Optimizer (a method to optimize))
+#Then we do run
+
+#For the class GIBinding using the Gobject Introspection bindings, two methods set and update are added.
+
 import os
 import shutil
 import yaml
@@ -67,13 +95,15 @@ class SystemCalculator():
         if mpi_run != '':
             mpi_run = mpi_run + ' ' + self.mpi + ' '
         #Build the command setting the number of omp threads
-        self.command =  'env OMP_NUM_THREADS='+self.omp+' '+mpi_run + os.environ['BIGDFT_ROOT']+'/bigdft'
+        self.command =  mpi_run + os.environ['BIGDFT_ROOT']+'/bigdft'
+        safe_print('Initialize a Calculator with OMP_NUM_THREADS=%s and command %s' % (self.omp,self.command) )
 
     def run(self, name='', outdir='', run_name='', input=None, posinp=None, skip=False):
-        """Run a calculation building the input file from a dictionary
-           a Logfile instance is returned."""
+        """Run a calculation building the input file from a dictionary.
+           a Logfile instance is returned.
+           The 'skip' option avoids to rerun the calculation."""
         # Set the number of omp threads
-        #os.environ['OMP_NUM_THREADS'] = self.omp
+        os.environ['OMP_NUM_THREADS'] = self.omp
         # Creating the yaml input file from a dictionary or another file
         if len(name) > 0:
             input_file = "%s.yaml" % name
@@ -81,6 +111,21 @@ class SystemCalculator():
         else:
             input_file = "input.yaml" #default name
             logname = "log.yaml"
+        # Copying the posinp input file if needed
+        if posinp != None:
+            #Check if the file does exist
+            assert os.path.exists(posinp)
+            if isinstance(input,dict):
+                #Add into the dictionary a posinp key
+                input['posinp'] = posinp
+                safe_print('Add a "posinp" key with the value "%s" into the dictionary input' % posinp)
+            else:
+                if len(name) > 0:
+                    fn,fn_ext = os.path.splitext(posinp)
+                    posinp_file = name+fn_ext
+                if posinp != posinp_file:
+                    shutil.copyfile(posinp,posinp_file)
+                    safe_print('Copying from "%s" the posinp file "%s"' % (posinp,posinp_file))
         if isinstance(input,dict):
             #Creating the yaml input file
             yaml.dump(input,stream=open(input_file,"w"),default_flow_style=False)
@@ -91,16 +136,6 @@ class SystemCalculator():
             if input != input_file:
                 shutil.copyfile(input,input_file)
                 safe_print('Copying from "%s" the yaml input file "%s"' % (input,input_file))
-        # Copying the posinp input file if needed
-        if posinp != None:
-            #Check if the file does exist
-            assert os.path.exists(posinp)
-            if len(name) > 0:
-                fn,fn_ext = os.path.splitext(posinp)
-                posinp_file = name+fn_ext
-                if posinp != posinp_file:
-                    shutil.copyfile(posinp,posinp_file)
-                    safe_print('Copying from "%s" the posinp file "%s"' % (posinp,posinp_file))
         # Adjust the command line with options
         command = self.command
         if len(name) > 0:
