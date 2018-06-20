9a660e8e5da1e8417cfd2af723be2de6c03133b8
Luigi Genovese <luigi.genovese@cea.fr>
2018-06-20 15:49:01 +0200

    corrections for compilations

--- bigdft/src/modules/openbabel_fake.c
+++ bigdft/src/modules/openbabel_fake.c
@@ -4,10 +4,6 @@
 #include <futile.h>
 //}
 
-void FC_FUNC_(openbabel_formats,OPENBABEL_FORMATS)(f90_dictionary_pointer* input_formats, f90_dictionary_pointer* output_formats)
-{
-}
-
 //extern "C" 
 void FC_FUNC_(openbabel_load, OPENBABEL_LOAD)(f90_dictionary_pointer *dict_posinp,
                                               const char *filename, unsigned int *flen)
